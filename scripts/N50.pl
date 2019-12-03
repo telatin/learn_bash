13f1e62e20d339eb3e22d7f4326dc0157830c1d7
Andrea Telatin <andrea@telatin.com>
2019-12-03 15:30:00 +0000

    Scripts update

--- scripts/N50.pl
+++ scripts/N50.pl
@@ -7,7 +7,7 @@ use Pod::Usage;
 use Term::ANSIColor  qw(:constants colorvalid colored);
 use Getopt::Long;
 use File::Basename;
-use JSON;
+use JSON::PP;
 
 local $Term::ANSIColor::AUTORESET = 1;
 
---
+++ scripts/weather.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+
+print STDERR "#STDERR# This script has been removed in the latest tutorial\n";
+
+print STDOUT "#STDOUT# This script has been removed and will no longer be used in the tutorial\n";
